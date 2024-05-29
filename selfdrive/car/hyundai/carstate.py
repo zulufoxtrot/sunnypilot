61a3e02e717d8e27d92971e228698ff7baf4a53b
zulufoxtrot <youri.lcd@gmail.com>
2024-05-29 21:55:33 +0200

    bugfix: set cruiseState to unavailable if limiter is active

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -112,7 +112,8 @@ class CarState(CarStateBase):
     # cruise state
     if self.CP.openpilotLongitudinalControl:
       # These are not used for engage/disengage since openpilot keeps track of state using the buttons
-      ret.cruiseState.available = cp.vl["TCS13"]["ACCEnable"] == 0 and self.mainEnabled
+      ret.cruiseState.available = cp.vl["TCS13"]["ACCEnable"] == 0 and self.mainEnabled \
+                                  and not cp.vl["E_EMS11"]["Cruise_Limit_Status"] == 1
       ret.cruiseState.enabled = cp.vl["TCS13"]["ACC_REQ"] == 1
       ret.cruiseState.standstill = False
       ret.cruiseState.nonAdaptive = False
