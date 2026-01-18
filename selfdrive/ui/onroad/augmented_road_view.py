8fd2348be1a42f7bca01987ee34a398770f2ce89
zulufoxtrot <youri.lcd@gmail.com>
2026-01-18 19:08:47 +0100

    disable camera preview

--- selfdrive/ui/onroad/augmented_road_view.py
+++ selfdrive/ui/onroad/augmented_road_view.py
@@ -86,10 +86,10 @@ class AugmentedRoadView(CameraView):
     )
 
     # Render the base camera view
-    super()._render(rect)
+    #super()._render(rect)
 
     # Draw all UI overlays
-    self.model_renderer.render(self._content_rect)
+    #self.model_renderer.render(self._content_rect)
     self._hud_renderer.render(self._content_rect)
     if not self.alert_renderer.render(self._content_rect):
       self.driver_state_renderer.render(self._content_rect)
